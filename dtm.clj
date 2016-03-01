a9029c323108f1fbeca33ad96460844a708b41b4
Paul Gowder <paul.gowder@gmail.com>
2016-02-29 19:14:20 -0600

    basic non-idiomatic dtm

--- dtm.clj
+++ dtm.clj
@@ -3,7 +3,6 @@
 (require '[clojure.string :as str])
 (def documents ["this is a cat" "this is a dog" "woof and a meow"])
 (def counts (map frequencies (map #(str/split % #" ") documents)))
-(require '[clojure.set :as set])
-; (def words (apply set/union (map #(str/split % #" ") documents)))
-
 (def words (distinct (apply concat (map #(str/split % #" ") documents))))
+(def zeroes (zipmap words (repeat 0)))
+(def dtm (map #(merge-with + % zeroes) counts))
