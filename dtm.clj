6b2feef3267e9a097c43ffb95c3308ed27cb9f16
Paul Gowder <paul.gowder@gmail.com>
2016-02-29 19:00:26 -0600

    ...

--- dtm.clj
+++ dtm.clj
@@ -4,4 +4,6 @@
 (def documents ["this is a cat" "this is a dog" "woof and a meow"])
 (def counts (map frequencies (map #(str/split % #" ") documents)))
 (require '[clojure.set :as set])
-(def words (apply set/union (map #(str/split % #" ") documents)))
+; (def words (apply set/union (map #(str/split % #" ") documents)))
+
+(def words (distinct (apply concat (map #(str/split % #" ") documents))))
