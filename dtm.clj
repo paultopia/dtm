aebe756a14f25c0fac28aa2fb9c25e01c942e061
Paul Gowder <paul.gowder@gmail.com>
2016-03-05 10:32:25 -0600

    cleanup names

--- dtm.clj
+++ dtm.clj
@@ -1,57 +1,57 @@
-; first draft of document term matrix--well, actually term document matrix.
+; first draft of term document matrix and preprocessing.
 
 (require '[clojure.string :as str])
 (require '[clojure.walk :as walk])
-(defn whitesplit 
-  "split a vector of string into vector of vectors of strings on whitespace" 
-  [docs] 
-  (map #(str/split % #" ") docs))
-(defn stringcounts 
+(defn whitespace-split 
+  "split a vector of strings into vector of vectors of strings on whitespace" 
+  [preprocessed-docs] 
+  (map #(str/split % #"\s") preprocessed-docs))
+(defn count-strings 
   "count frequencies of strings in vector of vectors of strings" 
   [stringvecs]
   (map frequencies stringvecs))   
-(defn liststrings 
+(defn list-strings 
   "list all strings in doc set" 
   [stringvecs]
   (distinct 
     (apply concat stringvecs)))
-(defn makezeroes 
+(defn cartesian-map 
   [stringlist] 
   (zipmap stringlist (repeat 0)))
-(defn expandcounts 
-  "based on strings in all docs, fill counts with 0 for unused strings in each single doc"
+(defn sparsify-counts 
+  "based on strings in all preprocesed docs, fill counts with 0 for unused strings in each single preprocessed doc"
   [zeroes counts]
   (map #(merge-with + % zeroes) counts))
-(defn bigmap
-  "split vector of docs by spaces then make zero-filled map of counts"
-  [docs]
-  (let [stringvecs (whitesplit docs)]
-    (expandcounts 
-      (-> stringvecs liststrings makezeroes) 
-      (-> stringvecs stringcounts))))  
-(defn terdocmmap
-  "make a sorted document-term-map of vector of docs with keywords"
-  [docs]
+(defn unsorted-TD-map
+  "split vector of preprocessed docs by spaces then make zero-filled map of counts"
+  [preprocessed-docs]
+  (let [stringvecs (whitespace-split preprocessed-docs)]
+    (sparsify-counts 
+      (-> stringvecs list-strings cartesian-map) 
+      (-> stringvecs count-strings))))  
+(defn TD-map
+  "make a sorted document-term-map of vector of preprocessed docs with keywords"
+  [preprocessed-docs]
   (walk/keywordize-keys  ; this is mainly for later flexibility
-    (map #(into (sorted-map) %) (bigmap docs))))
-(defn tdseqs
+    (map #(into (sorted-map) %) (unsorted-TD-map preprocessed-docs))))
+(defn TD-seqs
   "convert document-term-map into sequence of sequences"
   [tdmap]
   (cons 
     (keys (first tdmap))
     (map vals tdmap)))
-(defn nestvecify 
+(defn seqs-to-vecs 
   "sequence of sequences --> vector of vectors"
   [seqofseq]
   (into [] (map #(into [] %) seqofseq)))
-(defn termdocmatrix 
-  "make document term matrix as vector of vectors from vector of docs"
-  [docs]
-  (-> docs terdocmmap tdseqs nestvecify))
+(defn TD-matrix 
+  "make term document matrix as vector of vectors from vector of preprocessed docs"
+  [preprocessed-docs]
+  (-> preprocessed-docs TD-map TD-seqs seqs-to-vecs))
 
 ; example/test
 
-(termdocmatrix ["this is a cat" "this is a dog" "woof and a meow" "woof woof woof meow meow words"])
+(TD-matrix ["this is a cat" "this is a dog" "woof and a meow" "woof woof woof meow meow words"])
 
 ; produces [[:cat :is :this :words :dog :and :meow :woof :a] [1 1 1 0 0 0 0 0 1] [0 1 1 0 1 0 0 0 1] [0 0 0 0 0 1 1 1 1] [0 0 0 1 0 0 2 3 0]]
 
